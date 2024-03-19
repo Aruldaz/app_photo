1b861c1043053d69ee9a87d01c467b08e01ebd6e
Aruldaz <arulpris@gmail.com>
2024-03-19 21:07:07 +1200

    changed v2

--- libs/load.php
+++ libs/load.php
@@ -2,4 +2,19 @@
 function load_tempate($name){
     include __DIR__."/../_templates/$name.php";
 }
-?>
+
+function validate_credentials($username, $password){
+    if($username == "arul.satt@gmail.com" and $password == "Delecta")
+    {
+        return true;
+    } else
+    {
+        return false;
+    }
+
+
+}
+
+
+
+
