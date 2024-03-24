2fc806d74bf953a78c6dcffea07da8cac73bb4a3
Aruldaz <arulpris@gmail.com>
2024-03-24 21:17:05 +1200

    updated code

--- login.php
+++ login.php
@@ -5,69 +5,12 @@ include "libs/load.php";
 <!doctype html>
 <html lang="en" data-bs-theme="auto">
 
-<head>
-  <script src="assets/js/color-modes.js"></script>
-
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="">
-  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-  <meta name="generator" content="Hugo 0.122.0">
-  <title> Login || Photogram </title>
-
-
-
-
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
-
-  <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
-
-  <style>
-    .bd-placeholder-img {
-      font-size: 1.125rem;
-      text-anchor: middle;
-      -webkit-user-select: none;
-      -moz-user-select: none;
-      user-select: none;
-    }
-
-    @media (min-width: 768px) {
-      .bd-placeholder-img-lg {
-        font-size: 3.5rem;
-      }
-    }
-
-    html,
-    body {
-      height: 100%;
-    }
-
-    .form-signin {
-      max-width: 330px;
-      padding: 1rem;
-    }
-
-    .form-signin .form-floating:focus-within {
-      z-index: 2;
-    }
-
-    .form-signin input[type="email"] {
-      margin-bottom: -1px;
-      border-bottom-right-radius: 0;
-      border-bottom-left-radius: 0;
-    }
-
-    .form-signin input[type="password"] {
-      margin-bottom: 10px;
-      border-top-left-radius: 0;
-      border-top-right-radius: 0;
-    }
-  </style>
-
-
-</head>
+<?php
+    load_tempate('_head');
+    ?>
 
 <body>
+
 
     <?php
     load_tempate('_header');
