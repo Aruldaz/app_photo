5bb9977afa3a6846ac64a04050b5d091276a3326
Aruldaz <arulpris@gmail.com>
2024-03-24 21:19:52 +1200

    updated code

---
+++ signup.php
@@ -0,0 +1,33 @@
+<?php
+include "libs/load.php";
+?>
+
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+
+<?php
+    load_tempate('_head');
+    ?>
+
+<body>
+
+
+    <?php
+    load_tempate('_header');
+    ?>
+
+  <main>
+
+    <?php
+    load_tempate('_signup');
+    ?>
+  </main>
+
+  <?php
+  load_tempate('_footer');
+  ?>
+  <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
