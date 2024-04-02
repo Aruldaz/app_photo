7add668e1c29333dba8619fc7059c4b3bb1e2380
Aruldaz <arulpris@gmail.com>
2024-04-02 20:58:42 +1200

    edit

--- test.php
+++ test.php
@@ -1,13 +1,19 @@
 <pre>
 <?php
-print("_SERVER\n");
-print_r($_SERVER);
-print("_GET\n");
-print_r($_GET);
-print("_POST\n");
-print_r($_POST);
-print("_COKKIE\n");
-print_r($_COOKIE);
+include 'libs/load.php';
+// print("_SERVER\n");
+// print_r($_SERVER);
+// print("_GET\n");
+// print_r($_GET);
+// print("_POST\n");
+// print_r($_POST);
+// print("_COKKIE\n");
+// print_r($_COOKIE);
 
+if(signup("arul","123456","arul@gmail.com","0777779890")){
+echo "sucuss";
+}else{
+ echo "fails";
+}
 ?>
 </pre>
